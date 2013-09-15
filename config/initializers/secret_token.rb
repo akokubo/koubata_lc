8325a7336ab6fdc123007e950a28ae7db3cab793
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-09-16 03:59:27 +0900

    アプリケーション初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KoubataLc::Application.config.secret_key_base = '217a091db8d72e952ce3521051b2d81b300c8250a6a43f62b761ad5592ad28fbd8f54142cc88f967f1fadff77f4643085ebc7fbf510f0b57d790a97910d8c702'
