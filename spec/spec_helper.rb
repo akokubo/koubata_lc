e689bcae48f54845043fe475bb412545c1bb5130
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-09-16 04:03:20 +0900

    RSpecによるテストでCapybaraを使用する設定

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Capybaraを使用する設定
+  config.include Capybara::DSL
 end
