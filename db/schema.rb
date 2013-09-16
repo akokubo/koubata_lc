951dd53266c000997f06e5d833c80c5f8c47e97d
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-09-17 01:51:46 +0900

    Messageモデルの生成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130916154631) do
+ActiveRecord::Schema.define(version: 20130916165101) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 20130916154631) do
   end
 
   add_index "categories", ["name"], name: "index_categories_on_name", unique: true
+
+  create_table "messages", force: true do |t|
+    t.integer  "from_id"
+    t.integer  "to_id"
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["from_id"], name: "index_messages_on_from_id"
+  add_index "messages", ["to_id"], name: "index_messages_on_to_id"
 
   create_table "offerings", force: true do |t|
     t.integer  "user_id"
