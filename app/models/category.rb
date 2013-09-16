22beea843b2b2aeb9b996df307fddfe7676c56b2
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-09-16 15:48:14 +0900

    Categoryのname属性に存在と一意性の検証の追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ActiveRecord::Base
+  # name属性が存在し、唯一であることを検証
+  validates :name, presence: true, uniqueness: true
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # 事前に@categoryを作成する
+  before { @category = Category.new(name: "宿泊・賃貸住宅") }
+
+  # @categoryをテストの対象とする
+  subject { @category }
+
+  # name属性を持っている
+  it { should respond_to(:name) }
+
+  # 検証を通過する
+  it { should be_valid }
+
+  # name属性が存在しない場合
+  describe "when name is not present" do
+    # name属性を空に
+    before { @category.name = " " }
+
+    # 検証を通過しない
+    it { should_not be_valid }
+  end
+
+  # 同じname属性を持ったデータが存在していた場合
+  describe "when name is already taken" do
+    # 事前に同じname属性を持ったデータを用意し、保存を試みる
+    before do
+      category_with_same_name = @category.dup
+      category_with_same_name.save
+    end
+
+    # 検証を通過しない
+    it { should_not be_valid }
+  end
 end
