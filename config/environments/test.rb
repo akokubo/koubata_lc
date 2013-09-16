c3ed17250c74e14aee041af706bededd9ac9e6b1
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-09-16 17:09:00 +0900

    Userモデルのシードとテストを作成

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,8 @@ KoubataLc::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # deviseの確認メールの送信ホストの設定
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
