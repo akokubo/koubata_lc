1669a381d7b634ef96d1ee266b3217f78c7fa193
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-09-22 16:20:59 +0900

    homeのテスト

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,10 +1,43 @@
 require 'spec_helper'
 
 describe "Home Pages" do
-  describe "home page" do
-    it "should be contaion '幸畑プロジェクト'" do
-      visit root_path
-      expect(page).to have_content('幸畑プロジェクト')
-    end
+
+  it "should have the right links on the home page" do
+    visit root_path
+    click_link "お困りですか？"
+    expect(page).to have_title('お困りですか？')
+
+    visit root_path
+    click_link "幸畑プロジェクトとは"
+    expect(page).to have_title('幸畑プロジェクトとは')
+
+    visit root_path
+    click_link "お問い合わせ"
+    expect(page).to have_title('お問い合わせ')
   end
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+    it { should have_title(full_title('')) }
+    it { should have_title('幸畑プロジェクト') }
+    it { should_not have_title(' - ホーム') }
+  end
+
+  describe "Need Help page" do
+    before { visit need_help_path }
+    it { should have_title('お困りですか？') }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+    it { should have_title('幸畑プロジェクトとは') }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_title('お問い合わせ') }
+  end
+
 end
