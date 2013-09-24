c2cad316f752ea399ef870d42db48de8c52c1e65
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-09-24 20:16:17 +0900

    Ajaxを不使用に

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,3 +41,12 @@ jQuery.rails.handleMethod = function(link) {
   form.hide().append(metadata_input).appendTo('body');
   form.submit();
 };
+
+jQuery(document).bind('mobileinit', function () {
+  // console.log('mobileinit');
+  jQuery.extend(jQuery.mobile, {
+    loadingMessage: 'ロード中',
+    pageLoadErrorMessage: 'ページの読み込みに失敗しました',
+    ajaxEnabled: false
+  });
+});
