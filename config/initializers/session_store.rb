09a6db8d74a55dbb839b3ebd33064d98a17d3c8c
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-08-20 20:35:30 +0900

    アプリケーションモジュール名の変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_kec_session'
+Rails.application.config.session_store :cookie_store, key: '_koubata_lc_session'
