330cfa4d015ee274b698b285f9cacdf35902f2f4
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-05-05 17:41:30 +0900

    Paymentのメソッドの整理

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@ class AccountsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @payments = Payment.list(current_user.id)
-    @withs = Payment.withs(current_user.id)
+    @payments = Payment.find_by_from_id_or_to_id(current_user.id)
+    # @withs = Payment.withs(current_user.id)
   end
 end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,8 +3,8 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @payments = Payment.list(current_user.id)
-    @withs = Payment.withs(current_user.id)
+    @payments = Payment.find_by_from_id_or_to_id(current_user.id)
+    # @withs = Payment.withs(current_user.id)
   end
 
   def show
