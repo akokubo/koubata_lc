f5ad2e7738d8e44b7c5ec1fd7eccfd60d3e01c24
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-07-05 14:47:54 +0900

    エントリー/エントリー取り消しボタンの修正

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,8 +1,26 @@
 class Entry < ActiveRecord::Base
+#  before_validation :clear_hired_at
+
   belongs_to :task
   belongs_to :user
 
   # 必須属性の検証
   validates :task_id, presence: true
   validates :user_id, presence: true
+
+  def hired?
+    !hired_at.blank?
+  end
+
+=begin
+  def hired=(val)
+    @hired = val.in?([true, 1, '1'])
+  end
+
+  private
+
+    def clear_hired_at
+      self.hired_at = nil if @hired
+    end
+=end
 end
