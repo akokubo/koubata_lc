ef1f73af33c4cdf188824299ac269acaf2f6a213
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-07-20 15:57:36 +0900

    developmentのseedを変更

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -1,5 +1,5 @@
 names = %w(kokubo sakai kasiwaya kusibiki tsasaki kuji t-sachiko ksuga)
-japanese_names = %w(小久保温 坂井雄介 柏谷至 櫛引素夫 佐々木てる 久慈きみ代 田中志子 菅勝彦)
+japanese_names = %w(坂井雄介 柏谷至 櫛引素夫 小久保温 佐々木てる 久慈きみ代 田中志子 菅勝彦)
 
 0.upto(names.length - 1) do |idx|
   user = User.create(
@@ -26,3 +26,7 @@ Account.create(
   user_id: user.id,
   balance: 1000
 )
+
+user = User.find_by(name: '小久保温')
+user.admin = true
+user.save
