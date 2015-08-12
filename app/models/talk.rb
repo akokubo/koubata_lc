4d5f79c214e82b9f71e3dc1d1e295d72c3178ecc
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-08-12 23:01:33 +0900

    Talkの通知の実装

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,2 +1,19 @@
 class Talk < Message
+  after_save :notify
+
+  private
+
+  def notify
+    url = "/users/#{sender.id}/talks"
+    notification = Notification.find_by(user: recepient, url: url)
+    if notification.nil?
+      Notification.create!(
+        user: recepient,
+        body: "#{sender.name}さんからのメッセージがあります。",
+        url:  url
+      )
+    else
+      notification.update!(read_at: nil)
+    end
+  end
 end
