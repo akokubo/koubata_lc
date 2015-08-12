437a4bd8356378bd788f9453de63c9fb94e70639
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-08-12 16:03:55 +0900

    Notificationモデルに既読属性関係のメソッドを追加、ページネーションの個数を設定

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,18 @@ class Notification < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :body, presence: true
+
+  self.per_page = 10
+
+  def read?
+    if read_at.nil?
+      false
+    else
+      true
+    end
+  end
+
+  def read!
+    self.update!(read_at: Time.now)
+  end
 end
