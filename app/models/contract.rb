13d060cd3ef5fa8f11bb6720567e32d8074c7107
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-08-16 22:42:24 +0900

    Contract、Entrustの整理

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,19 +1,2 @@
 class Contract < Entry
-=begin
-  def offering
-    task
-  end
-
-  def offering=(object)
-    task_id = object.id
-  end
-
-  def offering_id
-    task_id
-  end
-
-  def offering_id=(object_id)
-    task_id = object_id
-  end
-=end
 end
--- app/models/entrust.rb
+++ app/models/entrust.rb
@@ -1,19 +1,2 @@
 class Entrust < Entry
-=begin
-  def want
-    task
-  end
-
-  def want=(object)
-    task_id = object.id
-  end
-
-  def want_id
-    task_id
-  end
-
-  def want_id=(object_id)
-    task_id = object_id
-  end
-=end
 end
