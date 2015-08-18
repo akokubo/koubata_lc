0a536a4be832264390df8a5703b330b76f1de50d
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-08-18 12:38:31 +0900

    Homeの統合テストの修正

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -9,6 +9,10 @@ describe 'Home Pages' do
     visit root_path
     click_link 'ログイン'
     expect(page).to have_title('ログイン')
+
+    visit root_path
+    click_link 'パスワード忘れはこちら'
+    expect(page).to have_title('パスワード忘れはこちら')
   end
 
   subject { page }
@@ -16,22 +20,26 @@ describe 'Home Pages' do
   describe 'Home page' do
     before { visit root_path }
     it { should have_title(full_title('')) }
+    it { should have_title(full_title('')) }
     it { should have_title('ハッピー幸畑たすけあい') }
     it { should_not have_title('ホーム - ') }
-  end
-
-  describe 'Need Help page' do
-    before { visit need_help_path }
-    it { should have_title('お困りですか？') }
   end
 
   describe 'About page' do
     before { visit about_path }
     it { should have_title('「ハッピー幸畑たすけあい」とは') }
+    it { should have_link('ホーム') }
   end
 
   describe 'Contact page' do
     before { visit contact_path }
     it { should have_title('お問い合わせ') }
+    it { should have_link('ホーム') }
+  end
+
+  describe 'Detailed page' do
+    before { visit detailed_path }
+    it { should have_title('詳しいしくみ') }
+    it { should have_link('ホーム') }
   end
 end
