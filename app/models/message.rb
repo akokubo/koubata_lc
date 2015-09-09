e25da277e605ff70cf098f3822bbec7f58b2100f
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-09-09 09:55:20 +0900

    Messageにscopeを使用

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,4 +8,21 @@ class Message < ActiveRecord::Base
   validates :body,         presence: true
   validates :sender_id,    presence: true
   validates :recepient_id, presence: true
+
+  scope :with, lambda { |user|
+    where(
+      'sender_id = :user_id or recepient_id = :user_id',
+      user_id: user.id
+    )
+  }
+
+  scope :between, lambda { |user1, user2|
+    query = 'sender_id = :user1_id and recepient_id = :user2_id'
+    query += ' or recepient_id = :user1_id and sender_id = :user2_id'
+    where(
+      query,
+      user1_id: user1.id,
+      user2_id: user2.id
+    )
+  }
 end
